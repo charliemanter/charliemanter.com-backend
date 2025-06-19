3cbd99d4f4c7d0b0fdb5ebf7d29367d08c399b0a
charliemanter <127909405+charliemanter@users.noreply.github.com>
2025-06-19 13:10:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://www.charliemanter.com"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
